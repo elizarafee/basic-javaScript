0d8a72cbbb9ca1bbdbb54e9b92bc4ff7c592b4ed
Eliza Ahmed <elizarafee@gmail.com>
2020-04-26 19:47:24 +0600

    Variables & Datatypes

---
+++ xampp/htdocs/javaScript/variablesDatatypes.js
@@ -0,0 +1,22 @@
+
+// datatype
+// Numeric 
+var num = 1, negnum = -1, int = 12.12, negint = -15.44;
+
+// String
+var string = "Eliza Hasan Rafee's portfolio!!!"
+var str = "He said \"I'm Hasan Mohammod Tareque\""
+
+// Boolean
+var a = true, b = false;
+
+// Null (Intensional absense of a value)
+var c = null;
+
+// Undefined (A placeholder)
+var d;
+
+console.log(num + ' ' + negint  + ' ' + str + ' ' + a + ' ' + c + ' ' + d);
+
+// knowing datatype
+console.log(typeof(d));
