bc4e133f8d13f2a1bca13a7cc199df11fb077cd0
Eliza Ahmed <elizarafee@gmail.com>
2020-04-30 21:07:45 +0600

    Loops

---
+++ xampp/htdocs/javaScript/loops.js
@@ -0,0 +1,18 @@
+
+var i;
+// For loop
+for(i = 10; i > 0 ; i--){
+    console.log('For loop no : ' + i);
+}
+
+var j = 0
+//  While loop
+while(j<10){
+    console.log('While loop no : ', + j);
+    j++;
+}
+
+var c = 1;
+do{
+    console.log('For while loop no : ' + c);
+}while(c>5);
