0b5be34e83a458252ecda7667e5d3a33cb66fc52
Eliza Ahmed <elizarafee@gmail.com>
2020-05-02 23:41:02 +0600

    Traversing an array by loop

--- xampp/htdocs/javaScript/loops.js
+++ xampp/htdocs/javaScript/loops.js
@@ -16,3 +16,10 @@ var c = 1;
 do{
     console.log('For while loop no : ' + c);
 }while(c>5);
+
+
+// Traversing an array by loop
+a = [10, 'eliza', true, 5.5, false, -20.54];
+for(i = 0; i < a.length; i++){
+    console.log(a[i]);
+}
