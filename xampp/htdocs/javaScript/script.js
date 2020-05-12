0cb6383fa79fc04f3f43871810e59a00a0ac4db0
Eliza Ahmed <elizarafee@gmail.com>
2020-05-13 00:41:05 +0600

    Pass arguments via Event Listener

--- xampp/htdocs/javaScript/script.js
+++ xampp/htdocs/javaScript/script.js
@@ -37,8 +37,8 @@ THEIMAGE.setAttribute("alt", "");
 // Apply iline CSS
 document.querySelector(".text h1").style.color = "red";
 document.querySelector(".text h1").style.backgroundColor = "black";
-document.querySelector(".text h2").style.cssText = "color: yellow; background-color: red; padding: 1em;";
-document.querySelector(".text h3").setAttribute("style", "color: orange; background-color: purple; font-size: 3rem");
+document.querySelector(".text h2").style.cssText = "color: yellow; background-color: red;";
+document.querySelector(".text h3").setAttribute("style", "color: orange; background-color: purple;");
 // Inline CSS is opinionated. so best practice is to create custom CSS file thenuse JS to manage those
 
 
@@ -52,15 +52,32 @@ A.classList.remove("hide");
 B.classList.add("hide");
 
 // After clicking on the link the page should not go on the top so we'll add e as a perameter and have to add e.preventDefault() in the function
-function reveal(e){
+// function reveal(e){
+//     e.preventDefault();
+//     A.classList.toggle("hide");
+//     B.classList.toggle("hide");
+// }
+
+// // A.onclick = reveal;
+// // B.onclick = reveal;
+
+// // Event handler work fine if we're only triggering functions based on DOM level events and then only trigger a single functions; so we'll use event listener.
+// A.addEventListener("click", reveal, false);  // not clear about the use of false
+// B.addEventListener("click", function(){console.log("CHANGED!!!");}, false);
+
+// Passing arguments via event listener
+
+function reveal(e, current){
     e.preventDefault();
-    A.classList.toggle("hide");
+    current.innerHTML == 'Click here' ? A.innerHTML = 'Oooops!' : A.innerHTML = 'Click here';
     B.classList.toggle("hide");
 }
 
-// A.onclick = reveal;
-// B.onclick = reveal;
+function rev(e, current){
+    e.preventDefault();
+    current.innerHTML == 'Lorem ipsum dolor sit amet.' ? B.innerHTML = 'CHANGED!!!' : B.innerHTML = 'Lorem ipsum dolor sit amet.';
+    A.classList.toggle("hide");
+}
 
-// Event handler work fine if we're only triggering functions based on DOM level events and then only trigger a single functions; so we'll use event listener.
-A.addEventListener("click", reveal, false);  // not clear about the use of false
-B.addEventListener("click", function(){console.log("CHANGED!!!");}, false);
+A.addEventListener("click", function(e){ reveal(e,this);}, false);  // not clear about the use of false
+B.addEventListener("click", function(e){ rev(e,this);}, false);
